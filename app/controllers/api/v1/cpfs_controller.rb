3ef637061793885243780ab088a2178cb4149f28
Mauro Rocha <mauror@mauror.com.br>
2021-03-18 21:00:59 -0300

    Validar CPF

--- app/controllers/api/v1/cpfs_controller.rb
+++ app/controllers/api/v1/cpfs_controller.rb
@@ -1,9 +1,15 @@
+require 'cpf_cnpj'
+
 module Api
   module V1
     class CpfsController < ApiController
       def show
-        result = NegativeList.blocked? params[:cpf]
-        render json: { blocked: result }, status: :ok
+        cpf = CPF.new(params[:cpf])
+        if cpf.valid?
+          result = NegativeList.blocked? params[:cpf]
+          return render json: { blocked: result }, status: :ok
+        end
+        render json: { error_message: 'CPF inválido' }, status: :bad_request
       end
     end
   end
