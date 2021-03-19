4c81e1c856ca39d5e5eed2f7fa9e0d749f40328e
christopher alves <chrisleo.usa@gmail.com>
2021-03-18 21:42:58 -0300

    limitado apenas rota index

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   namespace 'api', defaults: { format: :json } do
     namespace 'v1' do
-      resources :payment_methods
+      resources :payment_methods, only: [:index]
     end
   end
 end
