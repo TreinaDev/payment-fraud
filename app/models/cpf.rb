03fcc5113812bbe0de7361bed639e9750cb4ead9
RCOliveira98 <rcoliveira.ti@gmail.com>
2021-03-24 21:21:26 -0300

    refatora metodo qualify

--- app/models/cpf.rb
+++ app/models/cpf.rb
@@ -1,15 +1,11 @@
 class Cpf
   def self.qualify?(cpf)
     fraud_events = FraudEvent.where(cpf: cpf)
-    low_event = 0
+    return true if fraud_events.count >= 3
 
     fraud_events.each do |fraud_event|
       return true if fraud_event.event_severity == 1
-
-      low_event += 1
-      return true if low_event == 3
     end
-
     false
   end
 end
