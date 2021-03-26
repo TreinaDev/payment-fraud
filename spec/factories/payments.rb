6ec7915a3a348fe2524dee1b89e7795b384e211e
le-santos <lequinterio@gmail.com>
2021-03-25 22:27:50 -0300

    WIP atualiza seeds e factories

--- spec/factories/payments.rb
+++ spec/factories/payments.rb
@@ -1,10 +1,9 @@
 FactoryBot.define do
   factory :payment do
-    payment_method { nil }
-    cpf { 'MyString' }
-    customer_token { 'MyString' }
-    plan_id { 'MyString' }
+    payment_method { PaymentMethod.last || association(:payment_method) }
+    cpf { '12345678900' }
+    customer_token { 'a1s2d3' }
+    plan_id { '1' }
     status { :pending }
-    sequence(:payment_token) { |n| "token-#{n}" }
   end
 end
