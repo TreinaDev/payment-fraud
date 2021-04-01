d8d40b40e92c3b220d2e1eff3a0c8061325ae909
christopher alves <chrisleo.usa@gmail.com>
2021-03-31 21:11:03 -0300

    Conflitos na routes resolvidos

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,12 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  devise_for :users
   root 'home#index'
+  devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
 
   resources :payment_methods, only: %i[index new create show edit update] do
       post 'disable', on: :member
       post 'activate', on: :member
   end
-=======
-  root 'home#index'
 
-  devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
->>>>>>> 8ae71212b39f8e288e2728aefb18035d412a6683
 
   namespace 'api', defaults: { format: :json } do
     namespace 'v1' do
